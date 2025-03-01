f4d9bc0a637fa458269b88df3a7e304925863c6f
Rashi123ab <rashia729@gmail.com>
2025-03-01 16:10:22 +0530

    Added logger

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -4,15 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@ComponentScan(basePackages = "com.demo.component")//UC1
+@ComponentScan(basePackages = "com.demo.component")
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
+		logger.debug("Welcome to My Spring Concept Demo");
 		ApplicationContext context=SpringApplication.run(SpringConceptsApplication.class, args);
 		System.out.println("Checking Context:"+context.getBean(DemoBean.class));
+
+		logger.debug("Checking Context: {}",context.getBean (DemoBean.class));
+		logger.debug("\n*** Logging at debug level ***");
+
 	}
 
 }
+
+
+
+
