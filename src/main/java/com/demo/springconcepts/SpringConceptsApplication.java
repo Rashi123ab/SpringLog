0430c371cd3f39bf4988ace88fdef0227d56d8a1
Rashi123ab <rashia729@gmail.com>
2025-03-01 18:19:27 +0530

    Using EmployeeBean and DepartmentBean

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,18 +1,25 @@
 package com.demo.springconcepts;
 import com.demo.component.DemoBean;
-import org.springframework.boot.SpringApplication;
+import com.demo.component. EmployeeBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot. SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.demo.component")//UC1
+@ComponentScan(basePackages = "com.demo.component")
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger =LoggerFactory.getLogger (SpringConceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
-		ApplicationContext context=SpringApplication.run(SpringConceptsApplication.class, args);
-		System.out.println("Checking Context:"+context.getBean(DemoBean.class));
-	}
-
+		logger.debug("Welcome to My Spring Concept Demo");
+		ApplicationContext context =SpringApplication.run (SpringConceptsApplication.class, args);
+		logger.debug("Checking Context: {}",context.getBean (DemoBean.class));
+		logger.debug("\n*** adding EmployeeBean and DepartmentBean ***");
+		EmployeeBean employeeBean =context.getBean(EmployeeBean.class);
+		employeeBean.setEid(100);
+		employeeBean.setEname("Spring Framework");
+		employeeBean.showEmployeeDetails();
+}
 }
