0fb6fe26a0557961f75618d6e74ba4b13ec4460e
Rashi123ab <rashia729@gmail.com>
2025-03-01 18:43:33 +0530

    DemoBean.java

--- src/main/java/com/demo/component/DemoBean.java
+++ src/main/java/com/demo/component/DemoBean.java
@@ -1,6 +1,10 @@
 package com.demo.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoBean {
+    private static final Logger logger = LoggerFactory.getLogger(DemoBean.class);
 }
